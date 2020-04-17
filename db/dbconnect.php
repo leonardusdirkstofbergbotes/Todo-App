42de978751b8e6d6feb28a8e1edd309f3c4c70a4
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-04-17 12:14:28 +0200

    added a script that connects to my local database

---
+++ db/dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+    $DB_SERVER = "localhost:3308";
+    $DB_USERNAME = "root";
+    $DB_PASSWORD = "";
+    $DB_NAME = "todo";
+
+    $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+?>
