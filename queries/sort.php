a586511cd9f132fca2a99882c47a7820897cb4bf
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-04-20 09:00:33 +0200

    created a script that will sort the notes alphabetically

---
+++ queries/sort.php
@@ -0,0 +1,6 @@
+<?php
+        session_start();
+        $id = $_SESSION['id'];
+        $pull = "SELECT * FROM notes WHERE notes.user = $id ORDER BY title ASC";
+        header("Location: ../todo.php? pull= $pull");
+?>
